8765c3d9458af42584f656e50bbd76451830a061
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-08-30 00:24:04 +0300

    create service and model

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+import { Task, TaskStatus } from './tasks.model';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuidv4(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+}
