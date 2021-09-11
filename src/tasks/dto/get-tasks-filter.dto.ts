d7e619983cb624d9d3744772798867639030ae57
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-09-12 02:29:23 +0300

    get-task-filter add in class-validator

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,7 +1,13 @@
 /* eslint-disable prettier/prettier */
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { TaskStatus } from '../tasks.model';
 
 export class GetTaskFilterDto {
+  @IsOptional()
+  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
