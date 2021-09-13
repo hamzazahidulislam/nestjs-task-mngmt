4807f54a8b41f87e994494c52ac089e374614f90
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-09-14 02:48:05 +0300

    complete the database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig), TasksModule],
   controllers: [],
   providers: [],
 })
