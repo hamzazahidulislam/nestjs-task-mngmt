90de6083624304af10f9b0c122ecc1709e62684a
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-09-21 07:21:33 +0300

    create auth credentials dto

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,8 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+  async sgnUp(authCredentialsDto: AuthCredentialsDto) {
+    const { username, password } = authCredentialsDto;
 
+    const user = new
+  }
 }
