5c64be2d024aa963713e8e7f4f2bfeeefe42adeb
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-09-24 09:30:11 +0300

    jwt error

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,6 +8,8 @@ import {
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -29,5 +31,7 @@ export class AuthController {
 
   @Post('/test')
   @UseGuards(AuthGuard())
-  test() {}
+  test(@GetUser() user: User) {
+    console.log(user);
+  }
 }
